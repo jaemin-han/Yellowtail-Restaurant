759a9d83f2fef430982560dae4d7cd0847441628
Jaemin Han <jaemin.han@nyu.edu>
2016-12-03 17:57:04 -0500

    yellotail restructure

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(function(){
+
 	// initalizing the first image to class 'top'
 	$('.slides img:first').addClass('top');
 	//function to alter the image index by changing the class
     var change = function (){
      		var curr = $('.slides img.top');
-			var next = curr.next(); 
+			var next = curr.next();
 
-			// if the next image is not available then 
+			// if the next image is not available then
 			// set the class-top to the first image
 			// then vanish the current image to
 			// show the previous next image
@@ -14,13 +15,13 @@ $(function(){
 
 				next = $('.slides img:first');
 				next.addClass('top');
-		        
+
 		        curr.animate({opacity: 0.0},1700, function() {
 		            curr.removeClass('top');
 		            curr.css({opacity: 1.0});
 		        });
 
-			}else{
+			} else {
 
 				// pick the next image
 				// set the opacity to 0
@@ -37,8 +38,6 @@ $(function(){
     // repeat the function execution for every 3 secs
     setInterval(change, 4000 );
 
-
-});
 
 //https://www.youtube.com/watch?v=_EGO-d9H184
 $(document).ready(function(){
@@ -67,34 +66,6 @@ document.getElementsByTagName("li")[0].addEventListener("click",function(){
 		this.parentNode.removeAttribute("class");
 	}
 });
-
-
-
-
-
-//https://www.youtube.com/watch?v=utonytGKodc (navigation map) - sticky navi map//
-// jQuery(document).ready(function() {
-
-// 	var navOffset = jQuery("nav").offset().top;
-
-// 	jQuery("nav").wrap('<div class="nav-placeholder"></div>');
-// 	jQuery(".nav-placeholder").height(jQuery("nav").outerHeight());
-
-// 	jQuery("nav").wrapInner('<div class="nav-inner"</div>');
-// 	jQuery(".nav-inner").wrapInner('<div class="nav-inner-most"</div>');
-
-// 	jQuery(window).scroll(function() {
-// 	var scrollPos = jQuery(window).scrollTop();
-
-// 	if (scrollPos >= navOffset) {
-// 		jQuery("nav").addClass ("fixed");
-// 	} else {
-// 		jQuery("nav").removeClass("fixed");
-// 	}
-// 	});
-
-
-// });
 
 //gallery switch -- http://katiecullinan.github.io/Cville-Coffee/
 $(document).ready(function(){
@@ -147,9 +118,8 @@ $(document).ready(function(){
 });
 
 
-
 //menu bars//
-$(document).ready(function(){	
+$(document).ready(function(){
     $('.example2').hide().before('<a href="#" id="toggle-example2" class="button">Open/Close</a>');
 	$('a#toggle-example2').click(function() {
 		$('.example2').slideToggle(1000);
@@ -157,11 +127,18 @@ $(document).ready(function(){
 	});
 });
 
+// Menu toggle
+$("#food-title").click(function() {
+  $(".food-menu").toggle();
+});
 
-//Second to last section//
-$(document).ready(function(){
-       $('.parallax-bottom').scrolly({bgParallax: true});
-    });
+$("#drink-title").click(function() {
+  $(".drink-menu").toggle();
+});
+
+$("#other-title").click(function() {
+  $(".other-menu").toggle();
+});
 
 
-
+});
